6c54b7ee52e1f470cc955a995995cbb66345c0a9
spriyanka2244 <spriyanka2244@gmail.com>
2023-05-19 02:53:07 +0530

    second commit

--- demo.js
+++ demo.js
@@ -1,1 +1,23 @@
-console.log("i love git")
+//examin the document object
+
+//console.dir(document);
+//console.log(document.URL);
+//console.log(document.title);
+//document.title =123
+//console.log(document.doctype);
+//console.log(document.head);
+//console.log(document.body);
+//console.log(document.querySelectorAll);
+
+var headerTitle =document.getElementById('header-title');
+//var header =document.getElementById('main-header');
+//console.log(headerTitle);
+headerTitle.style.borderBottom ='solid 3px #000';
+var headerTitle =document.getElementById('items');
+items.style.color="green";
+
+//get element by class name//
+//var items =document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent ='Hello 2';
