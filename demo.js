100db5581fdbb3799ab137fecfe18eb68cbb537e
spriyanka2244 <spriyanka2244@gmail.com>
2023-05-19 07:15:51 +0530

    change items

--- demo.js
+++ demo.js
@@ -7,18 +7,26 @@
 //console.log(document.doctype);
 //console.log(document.head);
 //console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[0]);
+//console.log(document.forms[0]);
+//console.log(document.links);
+//console.log(document.images);
 //console.log(document.querySelectorAll);
 
 //var headerTitle =document.getElementById('header-title');
 //var header =document.getElementById('main-header');
 //console.log(headerTitle);
-var headerTitle =document.getElementById('header-title');
-headerTitle.style.borderBottom ='solid 3px #a020f0';
-var headerTitle =document.getElementById('items');
-items.style.color="green";
+//var headerTitle =document.getElementById('header-title');
+//headerTitle.style.borderBottom ='solid 3px #a020f0';
+//var headerTitle =document.getElementById('items');
+//items.style.color="green";
+
+//var headerTitle =document.getElementById('header-titl
+
 
 //get element by class name//
-//var items =document.getElementsByClassName('list-group-item');
-//console.log(items);
-//console.log(items[1]);
-//items[1].textContent ='Hello 2';
+var items =document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent ='Hello 2';
