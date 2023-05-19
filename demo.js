b5d9cd90cb76db4a8dace03bf5a6ed1cd9458cdc
spriyanka2244 <spriyanka2244@gmail.com>
2023-05-19 07:34:32 +0530

    add item 5

--- demo.js
+++ demo.js
@@ -47,4 +47,6 @@ li[1].textContent='Hello 2';
 li[1].style.fontWeight='bold';
 li[1].style.backgroungColor='yellow';
 
-for(var i)
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor="#f4f4f4";
+}
