920233adfa910bbe6a49d0f19aa8e66ec6ab9129
spriyanka2244 <spriyanka2244@gmail.com>
2023-05-19 07:23:47 +0530

    font weight

--- demo.js
+++ demo.js
@@ -28,10 +28,12 @@
 //get element by class name//
 var items =document.getElementsByClassName('list-group-item');
 items[2].style.backgroundColor ='green';
-items.getElementsByClassName.fontWeight='bold';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
 
 //console.log(items);
 //console.log(items[1]);
 //items[1].textContent ='Hello 2';
-//items.getElementsByClassName.fontWeight='bold';
 //items[2].style.backgroundColor ='green';
