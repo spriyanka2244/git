c1790dd8ab7ec9d72414ed6ca33320eb97179863
spriyanka2244 <spriyanka2244@gmail.com>
2023-05-20 21:37:51 +0530

    filter

--- final.js
+++ final.js
@@ -6,6 +6,10 @@ form.addEventListener('submit', addItem);
 //delete event
 itemList.addEventListener('click',removeItem);
 
+//filter event
+filter.addEventListener('keyup',filterItems);
+
+
 //Add item
 function addItem(e){
     e.preventDefault();
@@ -31,6 +35,8 @@ function addItem(e){
 
     deleteBtn.appendChild(document.createTextNode('X'));
 
+
+
     //append button to li
     li.appendChild(deleteBtn);
 
@@ -46,4 +52,55 @@ function removeItem(e){
         }
     }
 }
+
+//edit button
+// function addItem(e){
+//     e.preventDefault();
+
+//     //get input value
+//     var newItem1 = document.getElementById('item').value;
+
+//     //create new li element
+//     var li = document.createElement('li');
+
+//     //add class
+//     li.className='list-group-item';
+//     console.log(li);
+
+//     //Add text node with input value
+//     li.appendChild(document.createTextNode(newItem1));
+
+//     //create del button element
+//     var EditBtn=document.createElement('button');
+
+//     //add class to del  button
+//     EditBtn.className='btn btn-dark btn-sm float-sm-right Edit';
+
+//     EditBtn.appendChild(document.createTextNode('='));
+
+
+
+//     //append button to li
+//     li.appendChild(EditBtn);
+
+//     itemList.appendChild(li);
+// }
+
+//filter items
+function filterItems(e){
+    //convertion lowercase
+    var text = e.target.value.toLowerCase();
+    var items =itemList.getElementsByTagName('li');
+
+    //convert to an array
+    Array.form(items).forEach(function(item){
+        var itemName =item.firstChild.textContent;
+        if(itemName.toLowerCase().indexof(text) != -1){
+            item.style.display = 'block';
+        } else{
+            item.style.display = 'none';
+        }   
+    });
+}
+
 
