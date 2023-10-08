5b6111e563b31d38bd916bd6a1a564ab95a52a56
spriyanka2244 <66846210+spriyanka2244@users.noreply.github.com>
2023-10-08 16:00:30 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,52 @@
-console.log("i love git")
+git hub comments
+
+e1cb1f5f3282d6fea76086df1d8df1d17921f10e 
+
+
+
+
+
+Why do we need headers any idea?
+everytime your going to make a request you want to  send something everytime when making request from front end to backend
+for above problem header is used In header you can add what you want to send when make a request
+example contenttype: application/json ou want to send everytime
+header is used to send which are common
+header is used for authentication 
+
+
+common error we are going wrong are
+
+axios request
+           url ,{} ,methodtype
+
+404 backend not understand what your saying
+don't use special charater
+
+
+What is axios?
+axios is a tool to communicate from front end to server
+front end  your laptop , server is cloud
+axios is used for network call
+
+What are the common problems faced when you make network calls and what should you do to solve it.
+url is incorrectly chosen
+ axios.post('url" ,(name:yash))
+not stringy
+send object correctly 
+url  -- baseurl ==>flipkart.com/ 
+
+                                        / getproducts =>  route some code behind the scenes
+                                        /buyproducts
+                                        /selproducts 
+
+
+url -> baseurl/route
+
+
+
+ open network tab and see what is wrong
+endpoint has expired, what to do/
+check the payload and the response tab
+if you are not able to still find 2 problems are possible code problem or request problem
+try whether it works with postman
+no need of stringifying the payload ever
